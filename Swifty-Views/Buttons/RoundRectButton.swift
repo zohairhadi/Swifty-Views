721bd4c201b6ee2032438d937eca080256355854
Syed Zohair Abbas Hadi <zohairabbashadi@hotmail.com>
2021-09-13 12:38:51 +0500

    inital commit

---
+++ Swifty-Views/Buttons/RoundRectButton.swift
@@ -0,0 +1,44 @@
+//
+//  RoundRectButton.swift
+//  Swifty Views
+//
+//  Created by Zohair on 09/09/2021.
+//
+
+import UIKit
+
+class RoundRectButton: UIButton {
+
+    required init(color: UIColor, titleColor: UIColor, borderColor: UIColor, titleString : String, size: CGFloat) {
+        super.init(frame: .zero)
+        translatesAutoresizingMaskIntoConstraints = false
+        self.layer.cornerRadius = 10
+        backgroundColor = color
+        setTitle(titleString, for: .normal)
+        setTitleColor(titleColor, for: .normal)
+        setTitleColor(titleColor.withAlphaComponent(0.2), for: .highlighted)
+        self.titleLabel!.font = UIFont.boldSystemFont(ofSize: size)
+        self.layer.borderWidth = 1
+        self.layer.borderColor = borderColor.cgColor
+    }
+
+    required init(color: UIColor, titleColor: UIColor, borderColor: UIColor, titleString : String, size: CGFloat, img: UIImage) {
+        super.init(frame: .zero)
+        translatesAutoresizingMaskIntoConstraints = false
+        self.layer.cornerRadius = 10
+        backgroundColor = color
+        setTitle(titleString, for: .normal)
+        setTitleColor(titleColor, for: .normal)
+        setTitleColor(titleColor.withAlphaComponent(0.2) , for: .highlighted)
+        setImage(img, for: .normal)
+        imageView?.tintColor = .black
+        self.imageEdgeInsets = UIEdgeInsets(top: 0, left:0, bottom: 0, right: 10)
+        self.titleLabel!.font = UIFont.boldSystemFont(ofSize: size)
+        self.layer.borderWidth = 1
+        self.layer.borderColor = borderColor.cgColor
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Swifty-Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swifty-Views
+//
+//  Created by Zohair on 13/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
