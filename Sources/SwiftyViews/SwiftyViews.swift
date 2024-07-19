14de0c715b4211384172bcb0dc19c596466b699c
Syed Zohair Abbas Hadi <45510142+zohairhadi@users.noreply.github.com>
2024-07-19 16:56:58 +0500

    feat: add round square button

--- Sources/SwiftyViews/SwiftyViews.swift
+++ Sources/SwiftyViews/SwiftyViews.swift
@@ -37,3 +37,23 @@ public class RoundRectButton: UIButton {
 		fatalError("init(coder:) has not been implemented")
 	}
 }
+
+
+class RoundSquareButton: UIButton {
+
+	required init(color: UIColor, titleColor: UIColor, borderColor: UIColor, borderWidth: CGFloat, size: CGFloat, img: UIImage) {
+		super.init(frame: CGRect(x: 0, y: 0, width: 70, height: 70))
+		translatesAutoresizingMaskIntoConstraints = false
+		self.layer.cornerRadius = 10
+		backgroundColor = color
+		setImage(img, for: .normal)
+		setImage(img, for: .highlighted)
+		imageView?.tintColor = .black
+		self.layer.borderWidth = borderWidth
+		self.layer.borderColor = borderColor.cgColor
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+}
