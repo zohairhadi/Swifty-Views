45dff26ae06155057fa53a34295bd0f82952da0c
aarijimam <35100854+aarijimam@users.noreply.github.com>
2024-07-19 15:41:35 +0500

    feat: Added buttons to package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Swifty-Views"),
+            name: "Swifty-Views",
+            path: "Sources"),
         .testTarget(
             name: "Swifty-ViewsTests",
             dependencies: ["Swifty-Views"]),
