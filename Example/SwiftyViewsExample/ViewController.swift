aa2748dda714e04f6efa8aa3bda40b8e3b38513b
Syed Zohair Abbas Hadi <45510142+zohairhadi@users.noreply.github.com>
2024-07-19 16:43:14 +0500

    initial commit

---
+++ Example/SwiftyViewsExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyViewsExample
+//
+//  Created by Syed Zohair Abbas Hadi on 7/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
