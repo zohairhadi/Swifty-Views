58729b2c0250fa895c55a3bb5bd6dd2db0df79c8
aarijimam <35100854+aarijimam@users.noreply.github.com>
2024-07-19 15:45:02 +0500

    refactor: Changed name of package

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "Swifty-Views",
+    name: "SwiftyViews",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Swifty-Views",
+            name: "SwiftyViews",
             targets: ["Swifty-Views"]),
     ],
     targets: [
