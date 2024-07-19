a44635e08c62a9d282a6b999df6d9a30766617c4
aarijimam <35100854+aarijimam@users.noreply.github.com>
2024-07-19 15:54:12 +0500

    refactor: Changed name of package

--- Package.swift
+++ Package.swift
@@ -9,16 +9,16 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftyViews",
-            targets: ["Swifty-Views"]),
+            targets: ["SwiftyViews"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Swifty-Views",
+            name: "SwiftyViews",
             path: "Sources"),
         .testTarget(
-            name: "Swifty-ViewsTests",
-            dependencies: ["Swifty-Views"]),
+            name: "SwiftyViewsTests",
+            dependencies: ["SwiftyViews"]),
     ]
 )
