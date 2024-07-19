5fd9dc10f085b85578545ffafda8a3e6b1eb8b48
aarijimam <35100854+aarijimam@users.noreply.github.com>
2024-07-19 15:56:54 +0500

    refactor: Renamed Package back

--- Package.swift
+++ Package.swift
@@ -4,21 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyViews",
+    name: "Swifty-Views",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SwiftyViews",
-            targets: ["SwiftyViews"]),
+            name: "Swifty-Views",
+            targets: ["Swifty-Views"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftyViews",
+            name: "Swifty-Views",
             path: "Sources"),
         .testTarget(
-            name: "SwiftyViewsTests",
-            dependencies: ["SwiftyViews"]),
+            name: "Swifty-ViewsTests",
+            dependencies: ["Swifty-Views"]),
     ]
 )
